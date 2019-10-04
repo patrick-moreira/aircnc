c83f0ce769cc0c9ee5222915cc9ab635befa0637
patrick-moreira <patrickmoreira1995@gmail.com>
2019-10-03 22:28:35 -0300

    feat: Login e Cadastro de um novo Spot/frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -14,8 +17,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-afwbi.mongodb.net/s
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edicao, delete)
 // req.body = Aessar corpo da requisicao (para criacao, edicao)
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
